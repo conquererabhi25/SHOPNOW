ce67f1b9c932995bb71c609d9d8299895b33d609
Abhijeet Koli <abhijeetkoli39@gmail.com>
2025-11-01 23:32:40 +0530

    Refactor App component: remove commented-out code and update cart route

--- shopnowapp/src/App.jsx
+++ shopnowapp/src/App.jsx
@@ -1,64 +1,8 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import HomePage from "./Pages/HomePage";
-// import CartPage from "./Pages/cart";
-// import NotFoundPage from "./Pages/NotFoundPage";
-// import Header from "./components/header";
-// import Footer from "./components/footer";
-// import Mobileandtablets from "./Pages/Mobileandtablets";
-// import Fashion from "./Pages/Fashion";
-// import Electonics from "./Pages/Electronics";
-// import Furniture from "./Pages/Furniture";
-// import Beauty from "./Pages/Beauty";
-// import Grocery from "./Pages/Grocery";
-// import ProductOverview from "./Pages/ProductOverview";
-// import ProductOverview1 from "./Pages/ProductOverviewFakeapi";
-// import SearchResults from "./Pages/SearchResults";
-// import Cart from "./Pages/Cart";
-// import CheckoutPage from "./Pages/CheckoutPage";
-// import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-
-// const App = () => {
-//   return (
-//     <div
-//       className="bg-gray-100 bg-no-repeat bg-cover overflow-x-hidden"
-//       style={{
-//         backgroundImage: `url('/backgroundimge.png')`,
-//         height: "fit-content",
-//         objectFit: "cover",
-//       }}
-//     >
-//       <Router>
-//         <Header />
-//         <Routes>
-//           <Route exact path="/" element={<HomePage />} />
-//           <Route path="/mobileandtablets" element={<Mobileandtablets />} />
-//           <Route path="/fashion" element={<Fashion />} />
-//           <Route path="/electronics" element={<Electonics />} />
-//           <Route path="/furniture" element={<Furniture />} />
-//           <Route path="/grocery" element={<Grocery />} />
-//           <Route path="/beauty" element={<Beauty />} />
-//           <Route path="/cart" element={<CartPage />} />
-//           <Route path="/product/:id" element={<ProductOverview />} />
-//           <Route path="/productfakeapi/:id" element={<ProductOverview1 />} />
-//           <Route path="/cart" element={<Cart />} />
-//           <Route path="/search" element={<SearchResults />} />
-//           <Route path="/checkout" element={<CheckoutPage />} />
-//           <Route path="*" element={<NotFoundPage />} />
-//         </Routes>
-//         <Footer />
-//       </Router>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import CartPage from "./Pages/cart";
 import NotFoundPage from "./Pages/NotFoundPage";
 import Header from "./components/header";
 import Footer from "./components/footer";
@@ -112,7 +56,7 @@ const App = () => {
           <Route path="/furniture" element={<Furniture />} />
           <Route path="/grocery" element={<Grocery />} />
           <Route path="/beauty" element={<Beauty />} />
-          <Route path="/cart" element={<CartPage />} />
+          {/* <Route path="/cart" element={<CartPage />} /> */}
           <Route path="/product/:id" element={<ProductOverview />} />
           <Route path="/productfakeapi/:id" element={<ProductOverview1 />} />
           <Route path="/cart" element={<Cart />} />
